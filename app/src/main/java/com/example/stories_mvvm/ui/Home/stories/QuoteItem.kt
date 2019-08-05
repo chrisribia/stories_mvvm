ec2b92f7e388cf3937e6b3871bdb2159171540e1
chrisribia <chrisribia@gmail.com>
2019-08-06 00:53:25 +0300

    add on click on item in recyclerview

--- app/src/main/java/com/example/stories_mvvm/ui/Home/stories/QuoteItem.kt
+++ app/src/main/java/com/example/stories_mvvm/ui/Home/stories/QuoteItem.kt
@@ -3,17 +3,26 @@ package com.example.stories_mvvm.ui.Home.stories
 import com.example.stories_mvvm.R
 import com.example.stories_mvvm.data.entity.db.Quote
 import com.example.stories_mvvm.databinding.ItemQuoteBinding
+import com.example.stories_mvvm.utils.toast
 import com.xwray.groupie.databinding.BindableItem
 
 class QuoteItem(
     private val quote: Quote
+
 ) : BindableItem<ItemQuoteBinding>(){
+
 
     override fun getLayout() = R.layout.item_quote
 
     override fun bind(viewBinding: ItemQuoteBinding, position: Int) {
         viewBinding.setQuote(quote)
 
+        viewBinding.root.setOnClickListener{
+        it.context.toast(quote.author + " " + quote.quote  )
+        }
 
     }
+
+
+
 }
